fd76b01af4c748c37d281c4c74608cd06eb5cf79
Julian Scholle <julian.scholle@iff.fraunhofer.de>
2018-07-16 12:28:54 +0200

    test vcpkg

--- include/omniconfig.h
+++ include/omniconfig.h
@@ -32,9 +32,9 @@
 #define __omniconfig_h__
 #include <omniORB4/acconfig.h>
 
-#define __linux__ 1
+#define __NT__ 1
 #define __x86_64__ 1
-#define __OSVERSION__ 2
+#define __OSVERSION__ 4
 
 #undef PACKAGE_BUGREPORT
 #undef PACKAGE_NAME
